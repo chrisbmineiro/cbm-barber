ba36b5884639a3a3ea42168f05fee5cc338c208a
Christopher <chrisbmineiro@gmail.com>
2024-08-09 18:45:08 -0300

    adicionando item de agendamento

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Input } from "./_components/ui/input"
 import { Header } from "./_components/ui/header"
 import { Button } from "./_components/ui/button"
-import { SearchIcon } from "lucide-react"
+import { Badge, SearchIcon } from "lucide-react"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar"
 
 const Home = () => {
   return (
@@ -19,7 +21,7 @@ const Home = () => {
           </Button>
         </div>
 
-        <div className="h-150px relative mt-6 w-full">
+        <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com CBM Barbers"
             src="/banner-01.png"
@@ -27,6 +29,29 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">CBM Barber</p>
+              </div>
+            </div>
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">09</p>
+              <p className="text-sm">18:30</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
