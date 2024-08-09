cb3d4a19d9e047393795b0dba92d9671b033681d
Christopher <chrisbmineiro@gmail.com>
2024-08-09 18:11:53 -0300

    adicionando Header component e banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
-import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input"
+import { Header } from "./_components/ui/header"
+import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
-export default function Home() {
+const Home = () => {
   return (
-    <Button>Teste</Button>
-  );
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Hello, World</h2>
+        <p>Sexta-feira, 09 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="h-150px relative mt-6 w-full">
+          <Image
+            alt="Agende nos melhores com CBM Barbers"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Home
